ed60981d16ce5095eed1cd6bd83f700fe3b8176b
ELETI SAMHITHA <eletisamhitha4@gmail.com>
2026-06-11 17:31:34 +0530

    Fix import path

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "../context/Authcontext";
 
 export default function AuthPage() {
   const { login, signup, googleSignIn } = useAuth();
